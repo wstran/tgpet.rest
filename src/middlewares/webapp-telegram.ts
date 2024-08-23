4b90a8516a742e13cf8987d40bff9230f2449267
sonintact <hktts474941@gmail.com>
2024-08-23 17:16:31 +0700

    1

--- src/middlewares/webapp-telegram.ts
+++ src/middlewares/webapp-telegram.ts
@@ -82,7 +82,7 @@ export default async function (req: Request, res: Response, next: NextFunction) 
     const dataCheckString = Array.from(params.entries()).sort().map(e => `${e[0]}=${e[1]}`).join('\n');
 
     const hmac = CryptoJS.HmacSHA256(dataCheckString, secretKey).toString(CryptoJS.enc.Hex);
-
+    console.log({ hmac, hash });
     if (hmac !== hash) {
         return res.status(403).json({ message: 'Invalid user data.' });
     };
